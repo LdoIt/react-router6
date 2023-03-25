025627b3823fe0d456e7f6f35d283e911652b3c2
Good <1204889603@qq.com>
2023-03-25 20:42:58 +0800

    react-router@6

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import { NavLink, useRoutes} from 'react-router-dom'
+import routes from './pages/routes'
+export default function App() {
+  const element = useRoutes(routes)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <div className="row">
+          <div className="col-xs-offset-2 col-xs-8">
+            <div className="page-header">
+              <h2>React Router Demo</h2>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-xs-2 col-xs-offset-2">
+            <div className="list-group">
+              <NavLink className={({isActive}) => isActive ? 'list-group-item selected': 'list-group-item'} to="/about">
+                About
+              </NavLink>
+              {/* end只有一个active */}
+              <NavLink end className={({isActive}) => isActive ? 'list-group-item selected': 'list-group-item'} to="/home">
+                Home
+              </NavLink>          
+            </div>
+          </div>
+          <div className="col-xs-6">
+            <div className="panel">
+              <div className="panel-body">
+                {element}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
-
-export default App;
